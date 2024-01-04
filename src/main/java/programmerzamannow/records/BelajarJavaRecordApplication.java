8ad1978584c02a5846fc33f8d261d7d562c6c83c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-01-04 13:14:14 +0700

    Add initial application files and configurations
    This commit introduces a basic Maven Spring Boot application with a .gitignore file. Java 21 is specified as the required Java version in the pom.xml configuration. The application includes a main class, a basic test, and Maven wrapper files, enabling the project to be built with a specified version of Maven.

---
+++ src/main/java/programmerzamannow/records/BelajarJavaRecordApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/records/BelajarJavaRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
