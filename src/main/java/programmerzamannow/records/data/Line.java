e50fd1db3c3bf7fd2140ea494e30425c442e5b0b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-01-07 01:00:45 +0700

    Add Line record and update PointTest
    Introduced a new data structure, Line record, to store lines defined by two points. Updated PointTest class to include testing methodologies that cater to printing either Line or Point records. This ensures both Line and Point records are tested correctly and aids in better debugging.

---
+++ src/main/java/programmerzamannow/records/data/Line.java
@@ -0,0 +1,4 @@
+package programmerzamannow.records.data;
+
+public record Line(Point start, Point end) {
+}
--- src/test/java/programmerzamannow/records/data/PointTest.java
+++ src/test/java/programmerzamannow/records/data/PointTest.java
@@ -35,4 +35,24 @@ class PointTest {
     assertNotNull(Point.class.getConstructors()[0].getParameters()[0].getAnnotation(Valid.class));
     assertNotNull(Point.class.getConstructors()[0].getParameters()[1].getAnnotation(Valid.class));
   }
+
+  public void printObject(Object object){
+    if (object instanceof Line(Point(int startX, int startY), Point(int endX, int endY))){
+      System.out.println(startX);
+      System.out.println(startY);
+      System.out.println(endX);
+      System.out.println(endY);
+    }else if(object instanceof Point(int x, int y)){
+      System.out.println(x);
+      System.out.println(y);
+    } else {
+      System.out.println(object);
+    }
+  }
+
+  @Test
+  void beforeRecordPatterns() {
+    printObject(new Point(10, 10));
+    printObject(new Line(new Point(0, 0), new Point(10, 10)));
+  }
 }
